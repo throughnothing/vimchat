f8b0b5acaea7823c0d372feb4e7b14c291e4c2cb
naveedm9 <naveedm9@d1d492a2-f496-11dd-9d1d-173cc1d06e3c>
2009-07-19 21:39:47 +0000

    Added installation script and an example config file.
    git-svn-id: http://vimchat.googlecode.com/svn/trunk@140 d1d492a2-f496-11dd-9d1d-173cc1d06e3c

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+echo "Installing vimchat ..."
+
+mkdir -p ~/.vim/plugin 
+if [ $? != 0 ]; then echo "Could not create ~/.vim/plugin"; exit 1; fi
+cp plugin/vimchat.vim ~/.vim/plugin
+
+mkdir -p ~/.vim/syntax
+if [ $? != 0 ]; then echo "Could not create ~/.vim/syntax"; exit 1; fi
+cp syntax/vimchat.vim ~/.vim/syntax
+
+mkdir -p ~/.vimchat
+if [ $? != 0 ]; then echo "Could not create ~/.vimchat"; exit 1; fi
+
+if [ ! -f ~/.vimchat/icon.gif ]; then cp icon.gif ~/.vimchat/; fi
+
+if [ -f ~/.vimchat/config ]; then
+	cp config ~/.vimchat/config.example
+else
+	cp config ~/.vimchat/
+fi
+
+echo "Done :)"
